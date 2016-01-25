33aa199ef90821e09999728393c678a65a25b90d
Audre Staffen <audrewow@gmail.com>
2016-01-25 16:00:35 -0500

    Add WineItem and WineItemCollections classes and start of CSVProcessor and UserInterface classes.

---
+++ assignment1/WineItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class WineItem
+    {
+        // backing fields
+        private string id;
+        private string description;
+        private string pack;
+
+        // default constructor
+        public WineItem()
+        {
+
+        }
+
+        // 3-parameter constructor
+        public WineItem(string id, string description, string pack)
+        {
+            this.id = id;
+            this.description = description;
+            this.pack = pack;
+        }
+
+        // overrides ToString
+        public override string ToString()
+        {
+            return this.id + " " + this.description + " " + this.pack;
+        }
+
+        // properties for the backing fields
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Pack
+        {
+            get { return pack; }
+            set { pack = value; }
+        }
+    }
+}
---
+++ assignment1/WineItemCollections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class WineItemCollections
+    {
+        // backing field
+        private WineItem[] wineItem = new WineItem[500]; // filler array size
+
+        // default constructor
+        public WineItemCollections()
+        {
+
+        }
+    }
+}
